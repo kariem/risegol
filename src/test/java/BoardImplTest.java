cc913ab9bb473c108c68cd19f8098e34ccf3f49e
Michael F <amphied@hemera>
2012-08-10 17:52:12 +0200

    added test oscillating blinker

--- src/test/java/BoardImplTest.java
+++ src/test/java/BoardImplTest.java
@@ -19,7 +19,7 @@ public class BoardImplTest {
 	
 	}
 	@Test
-	public void testSolidBlock() {
+	public void testStillBlock() {
 		BoardImpl board = new BoardImpl(new StandardRule());
 		boolean[][] states = new boolean[2][2];
 		states[0][0]=true;
@@ -34,5 +34,21 @@ public class BoardImplTest {
 		assertTrue(nextRound[1][0]);
 		assertTrue(nextRound[1][1]);
 	}
-	
+
+	@Test
+	public void testOscillatingBlinker() {
+		BoardImpl board = new BoardImpl(new StandardRule());
+		boolean[][] states = new boolean[5][5];
+		states[1][2] = true;
+		states[2][2] = true;
+		states[3][2] = true;
+		board.set(states);
+		boolean[][] nextRound = board.tick();
+
+		assertTrue(nextRound[2][2]);
+		assertTrue(nextRound[2][1]);
+		assertTrue(nextRound[2][3]);
+		assertFalse(nextRound[1][2]);
+		assertFalse(nextRound[3][2]);
+	}
 }
